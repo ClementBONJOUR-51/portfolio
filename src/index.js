12551651c9a6aa66f525f3b9b7df2e5cecf08cb8
BONJOUR Clément <bonjourclement51100@gmail.com>
2022-05-05 23:34:52 +0200

    WIP home

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import Contact from './screens/Contact';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename='/portfolio'>
+  <BrowserRouter>
      <Routes>
         <Route path="/" element={ <Home/> } />
         <Route path="contact" element={ <Contact/> } />
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,17 +1,62 @@
-import './Home.css';
 import React from 'react';
-import { Link } from 'react-router-dom'
+import img_background from '../assets/img/background.jpg';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink
+} from 'reactstrap';
 
 function Home() {
+
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
   return (
-    <div className="Home">
-      <header className="App-header">
-        <p>
-          My Portfolio ! <code>WIP</code> Good visit
-        </p>
-        <Link to="/contact">Contact</Link>
-      </header>
-    </div>
+    <div className="bg-image" style={{ backgroundImage: `url(${img_background}`, backgroundSize: 'cover', backgroundPosition: 'center', height: "100vh" }}>
+      <Navbar light expand="md">
+        <NavbarBrand href="/">Portfolio</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav navbar className='d-flex justify-content-around ms-5' style={{ width: "80vw" }}>
+            <NavItem>
+              <NavLink href="/components/">ACCUEIL</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/components/">À PROPOS</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/components/">PPE</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/components/">STAGES</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/components/">VEILLE INFORMATIQUE</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/components/">RÉCENT TRAVAUX</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/components/">SUIVI SCOLAIRE</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/components/">CONTACT</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+      <div style={{ paddingTop: "40vh", paddingLeft: "30vh" }}>
+        <h1>Clément BONJOUR</h1>
+        <h5>Etudiant en informatique</h5>
+        {/* <Button outline size="lg" className='rounded-0 text-dark border-dark'>Découvrez moi</Button> */}
+        <button type="button" className="btn btn-outline-dark rounded-0 btn-lg">Découvrez moi</button>
+      </div>
+    </div >
   );
 }
 
