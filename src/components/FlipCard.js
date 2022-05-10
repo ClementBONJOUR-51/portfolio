9831d35653d7baa5f0be39a6169bd21d44e6c53b
BONJOUR Clément <bonjourclement51100@gmail.com>
2022-05-10 21:14:22 +0200

    WIP

---
+++ src/components/FlipCard.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './FlipCard.css';
+import img_bg_SPP from '../assets/img/bg_SPP.jpg';
+import { Row, Col, Button } from 'reactstrap';
+
+function FlipCard({ title, img_front, img_back }) {
+    return (
+
+        <div className="flip-card border-0">
+            <div className="flip-card-inner">
+                <div className="flip-card-front" style={{
+                    backgroundImage: `url(${img_front ? img_front : img_bg_SPP})`, backgroundPosition: "center",
+                    backgroundRepeat: "no-repeat", backgroundSize: "cover",
+                    boxShadow: "0px 0px 80px 0.05px grey"
+                }}>
+                </div>
+                <div className="flip-card-back" style={{
+                    backgroundImage: `url(${img_back})`, backgroundPosition: "center",
+                    backgroundRepeat: "no-repeat", backgroundSize: "cover",
+                    boxShadow: "0px 0px 80px 0.05px grey", color: "#212529"
+                }}>
+                    <h3 className='my-5'>{title}</h3>
+                    <Row>
+                        <Col><Button className='rounded-0 py-3 px-4 text-uppercase fw-bold mt-3' style={{ backgroundColor: "#212121" }}>Détails <span role="img" aria-label="hello">🔎</span></Button></Col>
+                        <Col><Button className='rounded-0 py-3 px-4 text-uppercase fw-bold mt-3' style={{ backgroundColor: "#212121" }}>Détails <span role="img" aria-label="hello">🔎</span></Button></Col>
+                    </Row>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default FlipCard;
--- src/components/PersonalWork.js
+++ src/components/PersonalWork.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { Container, Row, Col, Button } from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
 import img_line from '../assets/img/lines.svg';
 import img_bg_SPP from '../assets/img/bg_SPP.jpg';
+import FlipCard from './FlipCard';
+
 
 function PersonalWork() {
     return (
@@ -14,18 +16,27 @@ function PersonalWork() {
             <Container className="container text-center bg-white p-5" id='about' style={{ marginTop: "-100px", boxShadow: "0px 0px 80px 0.05px grey" }}>
                 <h1>Personal Works</h1>
                 <img src={img_line} alt="line" className="img-fluid mb-5" />
-                <Row>
-                    <Col className='px-5'>
-                        <h4>Géneration de carte</h4>
+                <Row className='my-5'>
+                    <Col className='px-2'>
+                        <FlipCard title={"Géneration de carte"} />
                     </Col>
-                    <Col className='px-5'>
-                        <h4>Paysage animé sans fin</h4>
+                    <Col className='px-2'>
+                        <FlipCard title={"Paysage animé sans fin"} />
                     </Col>
-                    <Col className='px-5'>
-                        <h4>Génération d'un Labyrinthe</h4>
+                    <Col className='px-2'>
+                        <FlipCard title={"Génération d'un Labyrinthe"} />
                     </Col>
                 </Row>
                 <Row>
+                    <Col className='px-2'>
+                        <FlipCard title={"Géneration de carte"} />
+                    </Col>
+                    <Col className='px-2'>
+                        <FlipCard title={"Paysage animé sans fin"} />
+                    </Col>
+                    <Col className='px-2'>
+                        <FlipCard title={"Génération d'un Labyrinthe"} />
+                    </Col>
                 </Row>
             </Container>
         </div>
