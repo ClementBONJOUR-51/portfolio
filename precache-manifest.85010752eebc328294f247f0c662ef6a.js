49d7675245005375e1b42b1ecaaeb347c0695924
Clément BONJOUR <bonjourclement51100@gmail.com>
2023-04-24 14:47:17 +0200

    Updates

---
+++ precache-manifest.85010752eebc328294f247f0c662ef6a.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "165a88786a7c54cea00a",
+    "url": "/portfolio/static/css/main.70710f78.chunk.css"
+  },
+  {
+    "revision": "165a88786a7c54cea00a",
+    "url": "/portfolio/static/js/main.1e09994c.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "f55bb0ca2f84d1debfd8",
+    "url": "/portfolio/static/css/2.edda3e2c.chunk.css"
+  },
+  {
+    "revision": "f55bb0ca2f84d1debfd8",
+    "url": "/portfolio/static/js/2.1d79e159.chunk.js"
+  },
+  {
+    "revision": "ff8fa44b76fbc12214bac67b765b931b",
+    "url": "/portfolio/static/media/lines.ff8fa44b.svg"
+  },
+  {
+    "revision": "58d83fa544865c04afc5349287b9c05a",
+    "url": "/portfolio/static/media/Clement-BONJOUR_Resume.58d83fa5.pdf"
+  },
+  {
+    "revision": "b2844aca6fc739a6c13fdf5483c6f47f",
+    "url": "/portfolio/static/media/background.b2844aca.jpg"
+  },
+  {
+    "revision": "00077c7f04a5dc5adf622bcf42036cc0",
+    "url": "/portfolio/static/media/BC.00077c7f.png"
+  },
+  {
+    "revision": "a6c2fc0e69d02a1efa9bad39208c0582",
+    "url": "/portfolio/static/media/dev.a6c2fc0e.png"
+  },
+  {
+    "revision": "2648a71ef9b89def5e13867461502ab5",
+    "url": "/portfolio/static/media/france.2648a71e.png"
+  },
+  {
+    "revision": "7ff5dacab96414cb75fff06a6be3064e",
+    "url": "/portfolio/static/media/royaume-uni.7ff5daca.png"
+  },
+  {
+    "revision": "0945439903385ac63d40286dbcee0b84",
+    "url": "/portfolio/static/media/profil.09454399.png"
+  },
+  {
+    "revision": "0347aed541c8045180c10ca44a541b74",
+    "url": "/portfolio/static/media/bg_skills.0347aed5.jpg"
+  },
+  {
+    "revision": "0d5a77a7c161d8ccc2ef4432b2800619",
+    "url": "/portfolio/static/media/SNCF.0d5a77a7.png"
+  },
+  {
+    "revision": "c612d82f914b84c53e9b444f129fdc05",
+    "url": "/portfolio/static/media/Albelli.c612d82f.svg"
+  },
+  {
+    "revision": "7c1638393879a95df96ab3a41b357d47",
+    "url": "/portfolio/static/media/bg_project.7c163839.jpg"
+  },
+  {
+    "revision": "15f0b57733137e5ee25ee7810dc7ef80",
+    "url": "/portfolio/static/media/bg_SPP.15f0b577.jpg"
+  },
+  {
+    "revision": "c42536bb2dcb9435c95171227971953f",
+    "url": "/portfolio/static/media/relief.c42536bb.jpg"
+  },
+  {
+    "revision": "fc4bc7ee5c8585c45cfcdf74d713edc5",
+    "url": "/portfolio/static/media/rainwindow.fc4bc7ee.jpg"
+  },
+  {
+    "revision": "e6ff521ff5731899e9ff073a89ffaea9",
+    "url": "/portfolio/static/media/maze.e6ff521f.jpg"
+  },
+  {
+    "revision": "f0e28a796a5d523ec6ef226070198e88",
+    "url": "/portfolio/static/media/joystick.f0e28a79.jpg"
+  },
+  {
+    "revision": "4904aaceffd525fa94269ad5517e080d",
+    "url": "/portfolio/static/media/flock.4904aace.jpg"
+  },
+  {
+    "revision": "50891262275d9ec935f86a0d23068f0a",
+    "url": "/portfolio/static/media/worldboss.50891262.jpg"
+  },
+  {
+    "revision": "156b25fa088b0f7e593402473e518ce4",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.7bb95f29ed0fd90674ae0f2a098a842d.js"
+  "/portfolio/precache-manifest.85010752eebc328294f247f0c662ef6a.js"
 );
 
 workbox.clientsClaim();
