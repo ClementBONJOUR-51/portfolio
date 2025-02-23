2bb999876f83b6ffd7ee0b20f96d97617200f42f
Clement BONJOUR <bonjourclement51100@gmail.com>
2025-02-23 13:50:52 +0100

    correction

---
+++ mail/contact.php
@@ -0,0 +1,20 @@
+<?php
+if(empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+  http_response_code(500);
+  exit();
+}
+
+$name = strip_tags(htmlspecialchars($_POST['name']));
+$email = strip_tags(htmlspecialchars($_POST['email']));
+$m_subject = strip_tags(htmlspecialchars($_POST['subject']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
+
+$to = "bonjour.clement.pro@gmail.com";
+$subject = "$m_subject:  $name";
+$body = "Vous avez reçu un nouveau message du formulaire de contact de votre site Web.\n\n"."Voici les détails :\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
+$header = "From: $email";
+$header .= "Reply-To: $email";	
+
+if(!mail($to, $subject, $body, $header))
+  http_response_code(500);
+?>
